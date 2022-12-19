6d9862f8bdd5ed47653a96ff1e0dd7d2e775dc8f
kaliraj <kalisdvk96@gmail.com>
2022-12-19 21:12:35 +0530

    update array item

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let i=parseInt(prompt("Enter the index of an array"));
+const foods=["chicken","mutton","fish","beef","parotta","briyani"];
+foods[i]=prompt("Enter the changeable item");
+console.log(foods);
